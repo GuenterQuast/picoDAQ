09a6b77b216f6b2014e748cd1297ca852c6ff566
Günter Quast <Guenter.Quast@online.de>
2017-09-30 15:46:45 +0200

    change to producer - consumer scheme with random and obligatory consumers

--- picoDAQ.py
+++ picoDAQ.py
@@ -38,6 +38,8 @@ import sys, time, json, threading
 import numpy as np, matplotlib.pyplot as plt
 import matplotlib.animation as anim
 
+from collections import deque
+
 from picoscope import ps2000a
 ps = ps2000a.PS2000a()  
 #from picoscope import ps4000
@@ -188,15 +190,25 @@ def picoIni():
   return ps
 # -- end def picoIni
 
-def getPicoData(ps):
-  global RUNNING, ibufw, ibufr, Ntrig, readrate, lifefrac
-#  print ('       !!! getPicoData starting')
-  Ntrig = 0 # count number of readings
-  ni = 0    # temporary variable
-  tlife = 0. # life time
-  readrate = 0.1
+def acquirePicoData(ps):
+  '''
+   Procucer Thread
+    
+     - collects data in buffers
+     - provedes all acquired data to exactly one consumer 
+
+   Communicates with consumer via collections.deque()
+
+  '''
+  global RUNNING, ibufr, Ntrig, readrate, lifefrac
+#  print ('       !!! acquirePicoData starting')
+  ibufw=-1     # buffer index
+  Ntrig = 0    # count number of readings
+  tlife = 0.   # life time
+  readrate = 0.
   lifefrac = 0.
 
+  ni = 0       # temporary variable
   T=Ns*dT # sampling period
 
   ts=time.time()
@@ -204,6 +216,10 @@ def getPicoData(ps):
   while RUNNING:
   # sample data from Picoscope handled by instance ps
     ibufw = (ibufw + 1) % NBuffers # next write buffer
+    while ibufw==ibufr:  # wait for consumer done with this buffer
+      if not RUNNING: return
+      time.sleep(0.001)
+      
     ps.runBlock(pretrig=pretrg) #
     # wait for PicoScope to set up (~1ms)
     time.sleep(0.001) # set-up time not to be counted as "life time"
@@ -223,14 +239,14 @@ def getPicoData(ps):
       #ps.getDataV(C, Ns, dataV=VBuf[ibufw,i], dtype=np.float32)
 
     Ntrig+=1
-    ibufr = ibufw # new data available, client will set to -1 when done
-    
-# wait for client acknowlege data received        
-    while ibufr >= 0: 
+    prod_q.append( (Ntrig, ibufw) )
+       
+# wait for free buffer       
+    while len(prod_q) == NBuffers:
       if not RUNNING: return
       time.sleep(0.001)
-
-    # calculate and display life time and read rate
+      
+# calculate life time and read rate
     if (Ntrig - ni) == 100:
       dt=time.time()-ts
       readrate = (Ntrig-ni)/dt
@@ -239,60 +255,159 @@ def getPicoData(ps):
       tlife = 0.
       ni=Ntrig
   # --- end while  
-  print ('          !!! getPicoData()  ended')
+  print ('          !!! acquirePicoData()  ended')
   return 0
-# -- end def getPicoData
+# -- end def acquirePicoData
+
+def getData():
+  '''main Consumer Thread, request data from procuder (acquirePicoData):
+
+       - provide all events for analysis
+       - provide subset of events to "random" consumers (picoVMeter, oszi)
+
+  '''
+  global ibufr # synchronize with producer via a global variable
+#              # should later be done as function call to a producer class
+  t0=time.time()
+  n0=0
+  n=0
+  while True:
+    while not len(prod_q): # wait for data in producer queue
+      time.sleep(0.001)
+    evNr, ibufr = prod_q.popleft()
+
+#  eventually, introduce random wait time to mimick processing time 
+#    time.sleep(np.random.randint(1, 25)/1000.)
+
+# analyze and re-distribute event data
+    l_obligatory=[]
+    for i, q in enumerate(request_qs):
+      if len(q):
+        req = q.popleft()
+        if req==0:                               # return poiner to Buffer      
+          consumer_qs[i].append( (evNr, ibufr) ) 
+          l_obligatory.append(i)
+        elif req==1:                               # return a copy of data
+          evTime=timeStamp[ibufr]
+          consumer_qs[i].append( (evNr, evTime, np.copy(VBuf[ibufr]) ) )
+          ibufr = -1  # signal we are done wiht this event
+        elif req==2:
+          evTime=timeStamp[ibufr]
+          consumer_qs[i].append( (evNr, evTime, np.copy(VBuf[ibufr]) ) )
+          l_obligatory.append(i)
+        else:
+          print('!!! getData: invalid mode', req)
+          exit(1)
+
+    if ibufr != -1: # wait for new request(s) by (all) obligatory  consumer(s)
+      while True:
+        done = True
+        for i in l_obligatory:
+          if len(request_qs[i]) == 0: done = False
+        if done: break
+        time.sleep(0.001)        
+#  now signal to producer that we are done with this event
+      ibufr = -1  # signal done with data
+
+# print event rate
+    n+=1
+    if n-n0 == 100:
+      print('evt %i:  rate: %.3gHz   life: %.2f%%' % (n, readrate, lifefrac))
+      if(evNr != n): print ("!!! ncnt != Ntrig: %i, %i"%(n,evNr) )
+      n0=n
+
+# -- end def getData
+
 
 def yieldEventCopy():
-# provide an event copy from getPicoData()
+# provide an event copy from getData()
    # this is useful for clients accessing only a subset of events
-  global ibufr
 # 
   evCnt=0
-  evData = np.empty([NChannels, Ns], dtype=np.float32 )
+#  evData = np.empty([NChannels, Ns], dtype=np.float32 )
   evTime = 0.
 #
+  rq = deque(maxlen=1)
+  request_qs.append(rq )
+  cq = deque(maxlen=1)
+  consumer_qs.append(cq )
+
+  evCnt=0
+  evTime = 0.
+
   while RUNNING:
-    while ibufr < 0: # wait for data
-      time.sleep(0.001)
+    rq.append(1)  # 1: request event data
+    while not len(cq):
+      time.sleep(0.01)
+    evNr, evTime, evData = cq.popleft()
+    #print('*==* yieldEventCopy: received event %i'%evNr)
     evCnt+=1
-    evTime=timeStamp[ibufr]
-    evData[:] = VBuf[ibufr] # take a copy of data
-    ibufr = -1  # signal data received to getPicoData()
+## client must set  ibufr = -1 if done ! 
     yield (evCnt, evTime, evData)
   
 def yieldEventPtr():
-# provide an pointer to event data from getPicoData()
+# provide a pointer to event data from acquirePicoData()
    # this is useful normal clients accessing all events
-  global ibufr
-# 
+  rq = deque(maxlen=1)
+  request_qs.append(rq )
+  cq = deque(maxlen=1)
+  consumer_qs.append(cq )
+
   evCnt=0
   evTime = 0.
+
   while RUNNING:
-    while ibufr < 0: # wait for data
-      time.sleep(0.001)
+    rq.append(0)  # 0: request an event pointer
+    while not len(cq):
+      time.sleep(0.01)
+    evNr, ibufr = cq.popleft()
+   # print('*==* yieldEventPtr: received event %i'%evNr)
     evCnt+=1
     evTime=timeStamp[ibufr]
     evData = VBuf[ibufr]
 ## client must set  ibufr = -1 if done ! 
     yield (evCnt, evTime, evData)
   
-def DAQtest():
-# test readout speed: do nothing, just request data from getPicoData
-  global ibufr
-  t0=time.time()
-  n0=0
-  n=0
+def obligatoryConsumer_test():
+  '''
+    test readout speed: do nothing, just request data from main consumer
+  '''
+#   set up communication queues
+  rq = deque(maxlen=1)
+  request_qs.append(rq )
+  cq = deque(maxlen=1)
+  consumer_qs.append(cq )
+
   while True:
-    while ibufr < 0:
-      time.sleep(0.001)
-    ibufr = -1  # signal data received
-# display frame rate
-    n+=1
-    if n-n0 == 100:
-      print('rate: %.3gHz   life: %.2f%%' % (readrate,lifefrac))
-      n0=n
-# -- end def DAQtest
+    rq.append(0)  # 0: event pointer
+    while not len(cq):
+      time.sleep(0.01)
+    evNr, ibufr = cq.popleft()
+    print('*==* obligatoryConsumer_test: received event %i'%evNr)
+
+#    introduce random wait time to mimick processing activity
+    time.sleep(np.random.randint(100,2000)/1000.)
+# - end def randomComsumer_test
+
+def randomConsumer_test():
+  '''
+    test readout speed: do nothing, just request data from main consumer
+  '''
+#   set up communication queues
+  rq = deque(maxlen=1)
+  request_qs.append(rq )
+  cq = deque(maxlen=1)
+  consumer_qs.append(cq )
+
+  while True:
+    rq.append(1)  # 0: event copy
+    while not len(cq):
+      time.sleep(0.01)
+    evNr, evTime, evData = cq.popleft()
+    print('*==* randomConsumer_test: received event %i'%evNr)
+#    introduce random wait time to mimick processing activity
+    time.sleep(np.random.randint(100,2000)/1000.)
+# - end def randomComsumer_test
 
 def VMeter():
 # Voltage measurement: average of short set of samples 
@@ -403,8 +518,8 @@ def VMeter():
       bgraph2.set_height(0.)
     animtxt.set_text(txt_t + '\n' + txt[0] + '\n' + txt[1])
 
-# !!! important if working wiht event pointer !!!
-    ibufr = -1  # signal data received, triggers next sample
+# !!! important if working with event pointer !!!
+###    ibufr = -1  # signal data received, triggers next sample
 #
     return (bgraph1,) + (bgraph2,) + graphs + (animtxt,)
 #- -end def animVMeter
@@ -475,15 +590,15 @@ def Oszi():
     global ibufr, n0 
     if n==1: n0=0
 
-    if n>0:    # !!! fix to avoid permanent display of first line in blit mode
+    if n>1:    # !!! fix to avoid permanent display of first line in blit mode
       for i, C in enumerate(picoChannels):
         graphs[i].set_data(samplingTimes, evData[i])
-#    else:
-#      for i, C in enumerate(picoChannels):
-#        graphs[i].set_data([],[])
+    else:
+      for i, C in enumerate(picoChannels):
+        graphs[i].set_data([],[])
 
 # !!! important if working with event pointer !!!
-    ibufr = -1  # signal data received, triggers next sample
+####    ibufr = -1  # signal data received, triggers next sample
 # display rate and life time
     if n-n0 == 100:
       txt='rate: %.3gHz  life: %.0f%%' % (readrate, lifefrac)
@@ -494,12 +609,12 @@ def Oszi():
   if verbose>0: print(' -> initializing graphics')
   fig, axes = grOsziIni()
   nrep=10000
-  ani=anim.FuncAnimation(fig, animOszi, yieldEventPtr, interval=0.,
+  ani=anim.FuncAnimation(fig, animOszi, yieldEventCopy, interval=50,
                          init_func=animOsziIni, blit=True,
                          fargs=None, repeat=True, save_count=None)
    # save_count=None is a (temporary) workaround to fix memory leak in animate
   plt.show()
-#     
+# - end def oszi     
     
 if __name__ == "__main__": # - - - - - - - - - - - - - - - - - - - - - -
 
@@ -510,33 +625,64 @@ if __name__ == "__main__": # - - - - - - - - - - - - - - - - - - - - - -
   Ns = nSamples
   # array of sampling times (in ms)
   samplingTimes =\
-   1000.*np.linspace(-pretrg*Ns*dT, (1.-pretrg)*Ns*dT,Ns)
+   1000.*np.linspace(-pretrg*Ns*dT, (1.-pretrg)*Ns*dT, Ns, endpoint=False)
 
   # reserve global space for data
-  NBuffers= 2
+  NBuffers= 10
   rawBuf = np.empty([NBuffers, NChannels, Ns], dtype=np.int16 )
   VBuf = np.empty([NBuffers, NChannels, Ns], dtype=np.float32 )
   timeStamp=np.empty(NBuffers)
-  ibufw = 0 # index of write buffer
-  ibufr = -1 # index of read buffer, -1 if not filled yet
+  ibufr = -1 # index of buffer being read
 
+  # initialize queues (collections.deque() for communication with threads
+  prod_q=deque(maxlen=NBuffers) # acquireData <-> getData
+  request_qs=[] # consumer request to getData
+                # 0:  request event pointer, obligatory consumer
+                # 1:  request event data, random consumer 
+                # 2:  request event data, obligatoray consumer
+  consumer_qs=[] # data from getData to consumer
+
+  # start data acquisition thread
   if verbose>0:
     print(" -> starting data acquisition thread")   
   RUNNING = True
+  thr_acquirePicoData=threading.Thread(target=acquirePicoData, args=(scope1,))
+  thr_acquirePicoData.daemon=True
+  thr_acquirePicoData.start()
 
-  thrPico=threading.Thread(target=getPicoData, args=(scope1,))
-  thrPico.daemon=True
-  thrPico.start()
+  # start main consumer thread
+  if verbose>0:
+    print(" -> starting main consumer thread")   
+  thr_getData=threading.Thread(target=getData)
+  thr_getData.daemon=True
+  thr_getData.start()
+
 #
 # --- infinite LOOP
   try:
     if mode=='test': # test readout speed
-      DAQtest()
+      randomConsumer_test()
     elif mode=='VMeter': # Voltmeter mode
-      VMeter()
-    else:
-      Oszi()
-
+      thr_VMeter=threading.Thread(target=VMeter)
+      thr_VMeter.daemon=True
+      thr_VMeter.start()
+      while True:
+        time.sleep(10.)
+    elif mode=='osci': # Voltmeter mode
+      thr_Oszi=threading.Thread(target=Oszi)
+      thr_Oszi.daemon=True
+      thr_Oszi.start()
+      while True:
+        time.sleep(10.)
+    elif mode=='demo': # Voltmeter mode
+      thr_Oszi=threading.Thread(target=Oszi)
+      thr_Oszi.daemon=True
+      thr_Oszi.start()
+      while True:
+        time.sleep(10.)
+      else:
+        print ('!!! no valid mode - exiting')
+        
   except KeyboardInterrupt:
 # END: code to clean up
     if verbose>0: print(' <ctrl C>  -> cleaning up ')
@@ -546,4 +692,4 @@ if __name__ == "__main__": # - - - - - - - - - - - - - - - - - - - - - -
     scope1.close()
     if verbose>0: print('                      -> exit ')
     exit(0)
-    
+  
