aca4366c6564583ba65b96be97033e6f20c65984
Guenter Quast <guenter.quast@online.de>
2019-05-01 09:53:29 +0200

    added saveFig button to mpRMeter

--- picodaqa/mpRMeter.py
+++ picodaqa/mpRMeter.py
@@ -14,8 +14,11 @@ matplotlib.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 if sys.version_info[0] < 3:
   import Tkinter as Tk
+  from tkFileDialog import asksaveasfilename
 else:
   import tkinter as Tk
+  from tkinter.filedialog import asksaveasfilename
+    
 import matplotlib.pyplot as plt, matplotlib.animation as anim
 
 # import RMeter class
@@ -45,25 +48,49 @@ def mpRMeter(Q, maxRate = 10. , interval = 2500., name='rate history'):
         yield evt
     except:
       print('*==* yieldEvt_fromQ: termination signal recieved')
-  
+
+#
+# --- define button actions 
+  def cmdSaveGraph(_event=None):
+    try:
+      filename = asksaveasfilename(initialdir='.',
+                                   initialfile='Fig_Rate.png', 
+                                   title='select file name')
+      if filename: figRM.savefig(filename) 
+    except Exception as e:
+      print(str(e))
+      pass
+      
 # ------- executable part -------- 
-#  print(' -> mpRMeter starting')
+  print(' -> mpRMeter starting')
 
-  try:
-    RM = RMeter(maxRate, interval, name)
-    figRM = RM.fig
+  RM = RMeter(maxRate, interval, name)
+  figRM = RM.fig
 
-# generate a simple window for graphics display as a tk.DrawingArea
-    root = Tk.Tk()
-    root.wm_title("Rate Display")
-    canvas = FigureCanvasTkAgg(figRM, master=root)
-    canvas.draw()
-    canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
-    canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
-    button = Tk.Button(master=root, text='Quit', command=sys.exit)
-    button.pack(side=Tk.BOTTOM)
+# generate a simple window with buttons for graphics display
+  root = Tk.Tk()
+  root.wm_title("Rate Display")
+
+  frame = Tk.Frame(master=root)
+  frame.grid(row=0, column=5)
+  frame.pack(padx=5, side=Tk.BOTTOM)
+   
+  button = Tk.Button(frame, text='Quit', command=sys.exit)
+  button.grid(row=0, column=5)
+  blank1 = Tk.Label(frame, width=5, text="")
+  blank1.grid(row=0, column=3)
+  buttonSvFig = Tk.Button(frame, text='saveFigure', 
+                            fg='purple', command=cmdSaveGraph)
+  buttonSvFig.grid(row=0, column=1)
+
+# set up window for graphics output
+  canvas = FigureCanvasTkAgg(figRM, master=root)
+  canvas.draw()
+  canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+  canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 
 # set up matplotlib animation
+  try:
     RMAnim = anim.FuncAnimation(figRM, RM, yieldEvt_fromQ, 
                         init_func=RM.init, interval=interval, blit=True,
                         fargs=None, repeat=True, save_count=None)
